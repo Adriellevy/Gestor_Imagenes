eb8d6a86b2238117f2a66b187ea92e5b8a62e19a
Adriel_Levy <adriellevy2014@gmail.com>
2026-07-13 19:10:17 -0300

    feat: agregar contenedores Docker para servidor, frontend web y base de datos

--- cliente-web/src/services/api.ts
+++ cliente-web/src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import type { Paciente, Internacion, Usuario, Pedido } from '../types';
 
-const API_URL = 'http://localhost:3000';
-//const API_URL = 'https://gestoy-imagenes.onrender.com';
+export const API_URL =
+  import.meta.env.VITE_API_URL || 'http://localhost:3002';
 
 const api = axios.create({
   baseURL: API_URL,
